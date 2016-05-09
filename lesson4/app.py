5ac08474ddb396369f04337b56301d38113f7ca5
adyouri <raryat@gmail.com>
2016-05-09 14:04:26 +0100

    Add flash messages

--- lesson4/app.py
+++ lesson4/app.py
@@ -1,7 +1,8 @@
 # -*- coding:utf8 -*-
-from flask import Flask, render_template, redirect, url_for, request, session
+from flask import Flask, render_template, redirect, url_for, request, session, flash
 import manage_db
 from functools import wraps
+
 
 def require_login(function):
     @wraps(function) 
@@ -9,11 +10,15 @@ def require_login(function):
         if 'logged_in' in session:
             return function(*args, **kwargs)
         else:
+            flash(u'سجّل دخولك أولا')
             return redirect(url_for('home'))
     return wrapper
 
+
 app = Flask(__name__)
 app.config.from_object('config')
+
+
 # Home Page
 @app.route("/")
 def home():
@@ -29,6 +34,7 @@ def create():
         title = request.form['title']
         content = request.form['content']
         manage_db.create(title, content)
+        flash(u'أضيف المقال بنجاح!')
     return redirect(url_for('home'))
 
 
@@ -44,7 +50,9 @@ def post(post_id):
 @require_login
 def delete(post_id):
     manage_db.delete(post_id)
+    flash(u'حُذف المقال بنجاح!')
     return redirect(url_for('home')) 
+
 
 # Login Route
 @app.route("/login", methods=['GET', 'POST'])
@@ -54,15 +62,18 @@ def login():
         password = request.form['password']
         if username == app.config['USERNAME'] and password == app.config['PASSWORD']:
             session['logged_in'] = True
+            flash(u'سُجّل دخولك بنجاح!')
         else:
             return redirect(url_for('home'))
     return redirect(url_for('home'))
+
 
 # Logout Route
 @app.route("/logout")
 @require_login
 def logout():
     session.pop('logged_in', None)
+    flash(u'سُجّل خروجك بنجاح!')
     return redirect(url_for('home'))
 
 
